279755466250a6630d6525bfeed25c925b22c490
Insu Jang <insujang@umich.edu>
2021-10-24 10:25:00 -0400

    Add basic tracer function (untested)

---
+++ pass/control-flow-tracer.cpp
@@ -0,0 +1,19 @@
+#include "control-flow-tracer.h"
+
+void ControlFlowTracer::incrementControlFlowCount(const llvm::DILocation *location) {
+  // Try to insert the given location with count 1.
+  auto result = counts_.insert({location, 1});
+  // If result.second == false, it means there already is an entry for the location.
+  // Increment the count.
+  if (result.second == false) {
+    result.first->second++;
+  }
+}
+
+int ControlFlowTracer::getControlFlowCount(const llvm::DILocation *location) const {
+  auto result = counts_.find(location);
+  if (result == counts_.end()) {
+    return -ENOENT;
+  }
+  return result->second;
+}
---
+++ pass/control-flow-tracer.h
@@ -0,0 +1,18 @@
+#ifndef __CONTROL_FLOW_TRACER_H__
+#define __CONTROL_FLOW_TRACER_H__
+
+#include "llvm/IR/DebugInfoMetadata.h"
+#include <map>
+#include <cerrno>
+
+class ControlFlowTracer {
+ public:
+  ControlFlowTracer() = default;
+  void incrementControlFlowCount(const llvm::DILocation *location);
+  int getControlFlowCount(const llvm::DILocation *location) const;
+
+ private:
+  std::map<const llvm::DILocation *, unsigned int> counts_;
+};
+
+#endif
