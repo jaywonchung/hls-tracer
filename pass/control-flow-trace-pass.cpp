2a84557d2f03d97dae512edd4689c31cd7746654
Insu Jang <insujang@umich.edu>
2021-10-26 10:01:30 -0400

    pass: add functionIR structure function in pass.

--- pass/control-flow-trace-pass.cpp
+++ pass/control-flow-trace-pass.cpp
@@ -1,32 +1,73 @@
+#include <string>
+#include <vector>
+#include "llvm/IR/DerivedTypes.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
 
 namespace {
+
+enum class TracerFunction : int {
+  IncrementCount,
+  GetCount,
+  Dump,
+};
+
 struct ControlFlowTracePass : public FunctionPass {
   static char ID;
   ControlFlowTracePass();
 
-  virtual bool runOnFunction(Function &func) override;
+  virtual bool doInitialization(Module& mod) override;
+  virtual bool runOnFunction(Function& func) override;
+
+ private:
+  Function* getFunctionIR(const TracerFunction func);
+
+ private:
+  Module* module_;
 };
 
 ControlFlowTracePass::ControlFlowTracePass()
-  : FunctionPass(ID) {}
+    : FunctionPass(ID), module_(nullptr) {}
 
-bool ControlFlowTracePass::runOnFunction(Function &func) {
+bool ControlFlowTracePass::doInitialization(Module& mod) {
+  module_ = &mod;
+  return false;
+}
+
+bool ControlFlowTracePass::runOnFunction(Function& func) {
   errs() << "Entered function " << func.getName() << ".\n";
 
-  for (auto fit = func.begin(), fend = func.end(); fit != fend; fit++) {
-    for (auto bit = fit->begin(), bend = fit->end(); bit != bend; bit++) {
-      const DILocation *loc = bit->getDebugLoc();
-      if (loc == nullptr) continue;
-      
+  // TODO: Find branch instructions (for control flow analysis)
+  // and add tracer functions with the corresponding llvm::DILocation *
+  // that can be achieved via llvm::Instruction::getDebugLoc()
+
+  auto tracerFunc = getFunctionIR(TracerFunction::IncrementCount);
+  return false;
+}
+
+Function* ControlFlowTracePass::getFunctionIR(const TracerFunction func) {
+  FunctionType* retType = nullptr;
+  std::vector<Type*> argTypes;
+  Function* func = nullptr;
+
+  switch (func) {
+    case TracerFunction::IncrementCount: {
+      break;
     }
+    case TracerFunction::GetCount:
+
+      break;
+    case TracerFunction::Dump:
+
+      break;
   }
 
-  return false;
+  return func;
 }
 
 }  // namespace
