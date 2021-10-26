7f6324946ee531f2a19b7fb2d9ea5f16595dbb6c
Insu Jang <insujang@umich.edu>
2021-10-26 16:40:30 -0400

    pass: change function extraction
    To call a function, we need llvm::Function*.
    Instead of creating it from FunctionType, I get it from existing function list.
    Still it is not working with Vitis clang, showing segmentation fault.
    Error is weird: LLVMSymbolizer: error reading file: No such file or directory.

--- pass/control-flow-trace-pass.cpp
+++ pass/control-flow-trace-pass.cpp
@@ -1,12 +1,13 @@
 #include <string>
 #include <vector>
-#include "llvm/IR/DerivedTypes.h"
+#include <map>
+#include <algorithm>
 #include "llvm/IR/Function.h"
-#include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/Casting.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Instructions.h"
 
 using namespace llvm;
 
@@ -21,63 +22,76 @@ enum class TracerFunction : int {
 struct ControlFlowTracePass : public ModulePass {
   static char ID;
   ControlFlowTracePass();
-
   virtual bool runOnModule(Module& module) override;
 
  private:
-  Function* getFunctionIR(Module& module, const TracerFunction tracerFunc);
+  Function* getTracerFunction(const TracerFunction tracerFunc);
+  int getTracerFunctions(Module::FunctionListType& functions);
+
+ private:
+  std::map<std::string, Function*> tracerFunctions;
 };
 
 ControlFlowTracePass::ControlFlowTracePass() : ModulePass(ID) {}
 
 bool ControlFlowTracePass::runOnModule(Module& module) {
+  bool changed = false;
   errs() << "Entered module " << module.getName() << ".\n";
+  getTracerFunctions(module.getFunctionList());
 
   // TODO: Find branch instructions (for control flow analysis)
   // and add tracer functions with the corresponding llvm::DILocation *
   // that can be achieved via llvm::Instruction::getDebugLoc()
 
-  auto tracerFunc = getFunctionIR(module, TracerFunction::IncrementCount);
-  return false;
-}
+  // TODO1: Insert a creation call of global ControlFlowTracer.
 
-Function* ControlFlowTracePass::getFunctionIR(Module& module,
-                                              const TracerFunction tracerFunc) {
-  Type* retType = nullptr;
-  std::vector<Type*> argTypes;
-  FunctionType* funcType = nullptr;
-  Function* func = nullptr;
 
-  switch (tracerFunc) {
-    case TracerFunction::IncrementCount:
-      /**
-       * Insu: testcode to create a member-function call with pointer arguments.
-       * Not working, so I commented them out.
-       * Leave it for now for study.
-       */
-      // retType = Type::getVoidTy(module.getContext());
-      // argTypes.push_back(PointerType::get(StructType::create(module.getContext(),
-      // "ControlFlowTracer"), 0));
-      // argTypes.push_back(PointerType::get(StructType::create(module.getContext(),
-      // "llvm::DILocation"), 0)); funcType = FunctionType::get(retType,
-      // argTypes, false); func = cast<Function>(
-      //     module.getOrInsertFunction("_ZN17ControlFlowTracer25incrementControlFlowCountEPKN4llvm10DILocationE",
-      //                                  funcType));
-      // if (func) {
-      //   func->print(errs());
-      // } else {
-      //   errs() << "func: nil\n";
-      // }
+  // TODO2: Using this value, call member function in our target code.
+  for (auto& func: module.getFunctionList()) {
+    if (func.getName().contains("sigma_n") == false) continue;
+    
+    for (auto &bb: func) {
+      for (auto bi = bb.begin(), bend = bb.end(); bi != bend; bi++) {
+        if (isa<BranchInst>(bi) == false) continue;
+        auto increaseCountTracerFunc = getTracerFunction(TracerFunction::IncrementCount);
+        std::vector<Value*> args;
+        // TODO: Put arguments.
+        Instruction* newInst = CallInst::Create(increaseCountTracerFunc, args);
+        bi->insertBefore(newInst);
+        changed = true;
+      }
+    }
 
-      break;
-    case TracerFunction::GetCount:
-
-      break;
-    case TracerFunction::Dump:
-
-      break;
+    break;
   }
 
+  return changed;
+}
+
+int ControlFlowTracePass::getTracerFunctions(Module::FunctionListType& functions) {
+  int function_num = 0;
+  for (auto& func: functions) {
+    if (func.getName().contains("ControlFlowTracer") == false) continue;
+    tracerFunctions.insert({func.getName(), &func});
+  }
+  return function_num;
+}
+
+Function* ControlFlowTracePass::getTracerFunction(const TracerFunction tracerFunc) {
+  Function* func = nullptr;
+  std::string key;
+  if (tracerFunc == TracerFunction::IncrementCount) key = "incrementControlFlowCount";
+  else if (tracerFunc == TracerFunction::GetCount) key = "getCount";
+  else if (tracerFunc == TracerFunction::Dump) key = "Dump";
+  else return nullptr;
+  
+  auto it = std::find_if(tracerFunctions.begin(), tracerFunctions.end(),
+              [&key](const std::pair<std::string, Function*>& element) -> bool {
+                return element.first.find(key) != std::string::npos;
+              });
+  if (it != tracerFunctions.end()) {
+    func = it->second;
+  }
   return func;
 }
 
