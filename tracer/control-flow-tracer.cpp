3e8e49f2bcc8d0e1c86b141be178057ee154d064
Insu Jang <insujang@umich.edu>
2021-11-13 20:10:47 -0500

    tracer: remove STL usage.

--- tracer/control-flow-tracer.cpp
+++ tracer/control-flow-tracer.cpp
@@ -1,8 +1,3 @@
-#include <map>
-#include <sstream>
-#include <string>
-#include <iostream>
-
 class ControlFlowTracer {
  public:
   ControlFlowTracer(): current_index_(0), buffer_size_(0), array_(nullptr) {};
@@ -18,13 +13,11 @@ class ControlFlowTracer {
 
 /* Should not be defined in class definition, otherwise inlined and cannot found in IR */
 void ControlFlowTracer::init(int *array, int size) {
-  std::cout << "Entered ControlFlowTracer init function" << std::endl;
   buffer_size_ = size;
   array_ = array;
 }
 
 void ControlFlowTracer::record(int row, int column) {
-  std::cout << "Entered ControlFlowTracer record function" << std::endl;
   array_[current_index_] = row;
   array_[current_index_ + 1] = column;
   current_index_ = (current_index_ + 2 ) % (buffer_size_ / sizeof(int));
