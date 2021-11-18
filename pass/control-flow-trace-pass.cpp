57c198fd7a0861a8ed2e906465119bcd30552513
Insu Jang <insujang@umich.edu>
2021-11-18 13:08:16 -0500

    Remove unnecessary headers

--- pass/control-flow-trace-pass.cpp
+++ pass/control-flow-trace-pass.cpp
@@ -10,10 +10,8 @@
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/Module.h"
-#include "llvm/IRReader/IRReader.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/Casting.h"
-#include "llvm/Support/SourceMgr.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
