1e61b7c0c92db33d8680067b57039d9804a89940
Do Yoon Kim <ddoyoon.kim@gmail.com>
2021-11-20 15:41:05 -0500

    Print if_prob

--- testfunctions/loop_test.cpp
+++ testfunctions/loop_test.cpp
@@ -7,14 +7,14 @@
 extern int top(int a[ARR_SZ], int, float);
 
 void run_test(int n, int ans, int *trace, float if_prob) {
-  printf("Running hot_loop(%d, trace)...\n", n);
+  printf("Running hot_loop(trace, %d, %.2f)...\n", n, if_prob);
   int out = top(trace, n, if_prob);
   printf("%s\n", "Function successfully returned. Content of trace array:");
   for (int i = 0; i < ARR_SZ; i++)
     printf("%c%d%c", " ["[i==0], trace[i], ",]"[i==ARR_SZ-1]);
   printf("\n");
   if (out != ans) {
-    printf("Expected hot_loop(%d, trace, %f) to be %d but got %d.\n", n, if_prob, ans, out);
+    printf("Expected hot_loop(trace, %d, %.2f) to be %d but got %d.\n", n, if_prob, ans, out);
     exit(1);
   }
 
